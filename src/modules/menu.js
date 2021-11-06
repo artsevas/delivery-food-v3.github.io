190e2c02e18d9561a534657d0b2e8d3a4ea437da
Artem Vorobev <v.a.vorobyov96@gmail.com>
2021-11-06 22:26:42 +0300

    connect webpack

---
+++ src/modules/menu.js
@@ -0,0 +1,18 @@
+const menu = () => {
+  const restourant = "pizza-plus";
+
+  const renderItems = (data) => {
+    console.log(data);
+  };
+
+  fetch(`./db/${restourant}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default menu
---
+++ src/modules/partners.js
@@ -0,0 +1,16 @@
+const partners = () => {
+  const renderItems = (data) => {
+    console.log(data);
+  };
+
+  fetch("https://delivery-food-v3-default-rtdb.firebaseio.com/db/partners.json")
+    .then((response) => response.json())
+    .then((data) => {
+      renderItems(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export default partners
